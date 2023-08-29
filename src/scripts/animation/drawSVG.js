5a7ad3a9b97d2dc276f9e602992e07cbb9969db4
Drew Hagni <drewhagni@gmail.com>
2023-08-29 08:21:19 -0500

    fixing logo and improving drawSVG animation

--- src/scripts/animation/drawSVG.js
+++ src/scripts/animation/drawSVG.js
@@ -17,8 +17,11 @@ export function drawSVG() {
         start: 'top 75%',
         toggleActions: 'restart none none reverse',
       }
-    })
+    }).addLabel('start')
 
-    drawSVGAnimTL.from(drawSVGElem.querySelectorAll('svg path'), {duration: 1, stagger: {each: 0.035, ease: 'sine.out'}, drawSVG: 0});  
+    drawSVGAnimTL
+      .from('#be-useful path', {duration: 1, stagger: {each: 0.05, ease: 'sine.out'}, drawSVG: 0})
+      .from('#be-kind path', {duration: 1, stagger: {each: 0.05, ease: 'sine.out'}, drawSVG: 0}, '-=0.75')
+      .from('#be-fearless path', {duration: 1, stagger: {each: 0.05, ease: 'sine.out'}, drawSVG: 0}, '-=1')
   }
 }
